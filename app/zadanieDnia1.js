382598b658b9f75a86bab5a9d05d81aefc97bdd0
Wojciech <olowek92@gmail.com>
2020-02-17 22:55:54 +0100

    Task-1 of a day done

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,14 @@
-//Twój kod
+const crypto = require("crypto");
+const fs = require("fs");
+
+process.argv.forEach((val, index) => {
+  if (index > 1) {
+    fs.readFile(val, "utf8", (err, data) => {
+      if (err === null) {
+        console.log(crypto.createHmac("sha256", data).digest("hex"));
+      } else {
+        console.log("Error: ", err);
+      }
+    });
+  }
+});
